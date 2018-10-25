11b3a3fa6dd55b970738a61a5135a66bad56ab67
mumy81 <muhammedcelik@gmail.com>
2018-10-25 13:40:48 +0300

    make User model and tests

--- Gemfile
+++ Gemfile
@@ -6,6 +6,7 @@ ruby '2.5.3'
 # Bundle edge Rails instead: gem 'rails', github: 'rails/rails'
 gem 'rails', '~> 5.2.1'
 #Boostrap-sass
+gem 'bcrypt'
 gem 'bootstrap-sass', '3.3.7'
 # Use Puma as the app server
 gem 'puma', '~> 3.11'
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_10_24_082730) do
+ActiveRecord::Schema.define(version: 2018_10_25_101005) do
 
   create_table "microposts", force: :cascade do |t|
     t.text "content"
@@ -25,6 +25,7 @@ ActiveRecord::Schema.define(version: 2018_10_24_082730) do
     t.string "password"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string "password_digest"
   end
 
 end
