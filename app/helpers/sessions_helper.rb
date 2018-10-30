efecfaf35590e6a3bb65cbd5ef1781865dfb58a1
mumy81 <muhammedcelik@gmail.com>
2018-10-30 09:43:16 +0300

    Implement advanced login (remember me, forget)

--- app/helpers/sessions_helper.rb
+++ app/helpers/sessions_helper.rb
@@ -7,9 +7,16 @@ module SessionsHelper
 
       # Returns the current logged-in user (if any).
 def current_user
-        if session[:user_id]
-            @current_user ||= User.find_by(id: session[:user_id])
-        end
+  if session[:user_id]
+    @current_user ||= User.find_by(id: session[:user_id])
+  elsif cookies.signed[:user_id]
+   # raise       # The tests still pass, so this branch is currently untested.
+    user = User.find_by(id: cookies.signed[:user_id])
+    if user && user.authenticated?(cookies[:remember_token])
+      log_in user
+      @current_user = user
+    end
+  end
       end
 
 
@@ -18,10 +25,27 @@ def current_user
     !current_user.nil?
   end
 
+    # Forgets a persistent session.
+    def forget(user)
+      user.forget
+      cookies.delete(:user_id)
+      cookies.delete(:remember_token)
+    end
+    
   def log_out
+    forget(current_user)
     session.delete(:user_id)
     @current_user = nil
   end
+
+  def remember(user)
+    user.remember
+    cookies.permanent.signed[:user_id] = user.id
+    cookies.permanent[:remember_token] = user.remember_token
+  end
+
   
+
+
       
 end
